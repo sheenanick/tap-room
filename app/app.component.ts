d9c75729c86f548c627c2235c1a6c09068697e4d
Sheena Nickerson and Timothy Herold <sheena+timothy@>
2016-10-17 15:19:46 -0700

    Add new project

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>My Second Angular 2 App</h1>
+
+  </div>`
+  })
+
+  export class AppComponent {
+
+  }
